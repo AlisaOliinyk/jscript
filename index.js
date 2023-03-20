b63b8ac321c2458998a9fdf393ad56211fbb7830
AlisaOliinyk <alisoliinyk@gmail.com>
2023-03-20 17:25:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let productName = "Droid";
-let pricePerItem = 2000;
+// let productName = "Droid";
+// let pricePerItem = 2000;
 
-// Change code below this line
-let productName = productName.replace("Droid", "Repair droid") 
-let pricePerItem += 1500; 
+// // Change code below this line
+// let productName = productName.replace("Droid", "Repair droid") 
+// let pricePerItem += 1500; 
+
+
+
+function sayHi() 
+{console.log("Hello, this is my first function!");}
+sayHi();
