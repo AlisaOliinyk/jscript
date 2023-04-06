5ccb463e91cc8ca9e035f3ba20c57a3acd3e2028
AlisaOliinyk <alisoliinyk@gmail.com>
2023-04-06 14:37:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,35 @@
 
 
 
-function sayHi() 
-{console.log("Hello, this is my first function!");}
-sayHi();
+// function sayHi() 
+// {console.log("Hello, this is my first function!");}
+// sayHi();
 
 
-const friends = ["Mango", "Kiwi"];
-console.table(friends);
+// const friends = ["Mango", "Kiwi"];
+// console.table(friends);
+
+
+//  function getSubstring(string, length) {
+    const substring = "Hello world";
+    
+
+     console.log(substring.slice(0, 3));
+     console.log(substring.slice(0, 6));
+    console.log(substring.slice(0, 8));
+  console.log(substring.slice(0, 11));
+console.log(substring.slice(0, 0));
+ 
+
+
+//   return substring; }
+
+
+//  const productName = "Ремонтний дроїд";
+
+//  console.log(productName.includes("н")); // true
+// console.log(productName.includes("Н")); // false
+//  console.log(productName.includes("дроїд")); // true
+//  console.log(productName.includes("Дроїд")); // false
+// console.log(productName.includes("Ремонтний")); // true
+// console.log(productName.includes("ремонтний"));
